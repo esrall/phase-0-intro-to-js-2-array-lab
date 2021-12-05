2e6a19d5ed18c2be66bc800ec1eeefea29e7869f
Martha Solomon <marthsolomon@gmail.com>
2021-12-04 20:37:18 -0500

    completed

--- index.js
+++ index.js
@@ -1,33 +1,38 @@
 // Write your solution here!
-const cats = ["Milo","Otis","Garfield"];
-function logcats(names) {
-    console.log("cats")
-}
-function destructivelyAppendCat(name) {
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat () {
     cats.push("Ralph");
 }
-function destructivelyPrependCat(name) {
-    cats.unshift("Bob")
+
+function destructivelyPrependCat() {
+    cats.unshift("Bob");
 }
-function destructivelyRemoveLastCat(name) {
+
+function destructivelyRemoveLastCat() {
     cats.pop();
 }
-function destructivelyRemoveFirstCat(name) {
+
+function destructivelyRemoveFirstCat() {
     cats.shift();
 }
-function appendCat(name) {
-    const copyCats = [...cats , "Broom"]
-    return copyCats
+
+function appendCat() {
+    const newCats = [...cats, "Broom"];
+    return newCats;
 }
-function prependCat(name) {
-    const copyCats = ["Arnold", ...cats];
-    return copyCats;
+
+function prependCat () {
+    const newerCats = ["Arnold", ... cats];
+    return newerCats;
 }
-function removeLastCat() {
-    const copyCats = cats.slice(0, 2);
-    return copyCats
+
+function removeLastCat () {
+    const newerNewCats = cats.slice(0, 2);
+    return newerNewCats;
 }
-function removeFirstCat() {
-    const copyCats = cats.slice(1)
-    return copyCats
+
+function removeFirstCat () {
+    const newestNewerNewCats = cats.slice(1);
+    return newestNewerNewCats;
 }
